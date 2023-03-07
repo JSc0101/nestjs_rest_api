a116db879675b00b8b902891761c20eb6f8e5fd2
Sebastian009w <ia3266595@gmail.com>
2023-03-06 21:12:42 -0500

    connecion a mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { ProductController } from './products/product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/products'),
+  ],
   controllers: [AppController, ProductController],
   providers: [AppService],
 })
