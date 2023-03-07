764b79c0e004021e9ad6dbea0e0a29b591c29901
Sebastian009w <ia3266595@gmail.com>
2023-03-06 21:43:38 -0500

    se implementa el CRUD

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,4 +9,36 @@ export class ProductsService {
   constructor(
     @InjectModel('Product') private readonly productModel: Model<PRODUCT>,
   ) {}
+
+  async gets(): Promise<PRODUCT[]> {
+    const products = await this.productModel.find();
+    return products;
+  }
+
+  async create(createProductDTO: CreateProductDTO): Promise<PRODUCT> {
+    const product = new this.productModel(createProductDTO);
+    return await product.save();
+  }
+
+  async update(
+    productID: string,
+    createProductDTO: CreateProductDTO,
+  ): Promise<PRODUCT> {
+    const updateProduct = await this.productModel.findByIdAndUpdate(
+      productID,
+      createProductDTO,
+      { new: true },
+    );
+    return updateProduct;
+  }
+
+  async delete(productID: string): Promise<PRODUCT> {
+    const deleteProduct = await this.productModel.findByIdAndDelete(productID);
+    return deleteProduct;
+  }
+
+  async getById(productID: string): Promise<PRODUCT> {
+    const product = await this.productModel.findById(productID);
+    return product;
+  }
 }
