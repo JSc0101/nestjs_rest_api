b5d3789ac321f66350d3f7462f23d347399f7f5a
Sebastian009w <ia3266595@gmail.com>
2023-03-06 21:26:39 -0500

    definiendo modelos y documentos y interfaces

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   providers: [ProductsService],
 })
 export class ProductModule {}
